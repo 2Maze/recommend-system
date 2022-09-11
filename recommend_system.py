a8a74442a86ec100eed69dfa802b087c1d02ae7c
danil <justprogrammertoo@gmail.com>
2022-09-11 14:03:50 +0300

    Change sorting function

--- recommend_system.py
+++ recommend_system.py
@@ -12,14 +12,28 @@ def cosine_similarity(news_vector, user_vector):
     u_v = np.array(user_vector, dtype=int)
     return np.dot(n_v, u_v) / (np.linalg.norm(n_v) * np.linalg.norm(u_v))
 
-def sorting(news_dict, vector):
+def sorting(id_cat, vector):
     """
     Функция сортирует новости учитывая интересы пользователя
-    :param news_dict: Словарь категорий
+    :param id_cat: Словарь категорий
     :param vector: Вектор с чем будем сравнивать
     :return: Сортированный массив id новостей
     """
     metrics_dict = dict()
+    news_dict = {}
+
+    for cl in id_cat.items():
+        news_dict[cl[0]] = [cl[1].TableGames,
+                            cl[1].MusicConcert,
+                            cl[1].Ballet,
+                            cl[1].Opera,
+                            cl[1].Theatre,
+                            cl[1].Hackaton,
+                            cl[1].Cinema,
+                            cl[1].Vebinar,
+                            cl[1].CyberSport,
+                            cl[1].Festival]
+
 
     for id_news in news_dict:
         metrics_dict[id_news] = cosine_similarity(news_dict[id_news], vector)
